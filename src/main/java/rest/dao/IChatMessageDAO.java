0da7770af14e4ebd464f5b60514a7f3e3638f473
Pushkar <pushkar.moi@gmail.com>
2018-11-12 18:44:13 -0800

    v2

--- src/main/java/rest/dao/IChatMessageDAO.java
+++ src/main/java/rest/dao/IChatMessageDAO.java
@@ -8,8 +8,6 @@ import rest.request.ChatMessageFetchRequest;
 import java.util.List;
 
 public interface IChatMessageDAO {
-
-    // same request gives the same response (idempotency)
     List<ChatMessage> idempotencyCheck(ChatMessageFetchRequest chatMessageFetchRequest) throws LOException;
 
     // list of messages before this timestamp (can be NULL)
