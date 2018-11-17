1a24fda4a6a9a80754a244d9126acc32f6d3f073
sahil-here <32823720+sahil-here@users.noreply.github.com>
2018-11-17 11:52:20 -0800

    Revert "Chat REST"

--- src/main/java/rest/dao/ChatMessageDAO.java
+++ src/main/java/rest/dao/ChatMessageDAO.java
@@ -1,19 +1,13 @@
 package rest.dao;
 
 import com.google.inject.Inject;
-import exception.LOErrorCode;
 import exception.LOException;
 import io.dropwizard.hibernate.AbstractDAO;
-import org.hibernate.Query;
 import org.hibernate.SessionFactory;
+import rest.dao.entity.ChatFetch;
 import rest.dao.entity.ChatMessage;
-import rest.dao.entity.Event;
 import rest.request.ChatMessageFetchRequest;
 
-import javax.ws.rs.QueryParam;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class ChatMessageDAO extends AbstractDAO<ChatMessage> implements IChatMessageDAO {
@@ -23,40 +17,14 @@ public class ChatMessageDAO extends AbstractDAO<ChatMessage> implements IChatMes
         super(sessionFactory);
     }
 
+
     @Override
     public List<ChatMessage> idempotencyCheck(ChatMessageFetchRequest chatMessageFetchRequest) throws LOException {
-        List<ChatMessage> messages = new ArrayList<ChatMessage>();
-        try {
-            Query query = namedQuery("idempotencyCheckForMessage");
-            query.setParameter("event_id", chatMessageFetchRequest.getEventId());
-            query.setParameter("timestamp", chatMessageFetchRequest.getTimestamp());
-            query.setParameter("limit", chatMessageFetchRequest.getLimit());
-            Iterator it = query.iterate();
-            while (it.hasNext()){
-                messages.add((ChatMessage) it.next());
-            }
-            return messages;
-        }catch (Exception ex){
-            throw new LOException(500, LOErrorCode.INTERNAL_SERVER_ERROR.getName(),ex);
-        }
+        return null;
     }
 
     @Override
-    public List<ChatMessage> findMessagesByEvent(Long eventId, Timestamp timestamp, int limit) throws LOException {
-        List<ChatMessage> messages = new ArrayList<ChatMessage>();
-        try {
-            Query query = namedQuery("findMessageByEventId");
-            query.setParameter("event_id", eventId);
-            query.setParameter("timestamp", timestamp);
-            query.setParameter("limit", limit);
-            Iterator it = query.iterate();
-            while (it.hasNext()){
-                messages.add((ChatMessage) it.next());
-            }
-            return messages;
-        }catch (Exception ex){
-            throw new LOException(500, LOErrorCode.INTERNAL_SERVER_ERROR.getName(),ex);
-        }
+    public List<ChatMessage> findMessageByTimestamp(ChatFetch chatFetch) throws LOException {
+        return null;
     }
-
 }
